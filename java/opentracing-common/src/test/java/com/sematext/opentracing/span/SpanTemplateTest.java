f2f1cf9be06c3174d890c61c9add818831026efd
Nedim Šabić <rabbitstack7@gmail.com>
2017-08-08 22:16:56 +0200

    add new test. refactor to avoid initializing the tracer twice

--- java/opentracing-common/src/test/java/com/sematext/opentracing/span/SpanTemplateTest.java
+++ java/opentracing-common/src/test/java/com/sematext/opentracing/span/SpanTemplateTest.java
@@ -9,20 +9,38 @@ import org.junit.Test;
 
 public class SpanTemplateTest {
 
-    private TracerInitializer tracerInitializer;
+    private TracerInitializer tracerInitializer = new TracerInitializer(Tracers.ZIPKIN);;
 
     private SpanOperations spanOps = new SpanTemplate();
+    private static boolean setup = false;
 
     @Before
-    public void setup() {
-        tracerInitializer = new TracerInitializer(Tracers.ZIPKIN);
+    public void setUp() {
+        // JUnit 5 (@BeforeAll) would deal with this
+        if (setup) {
+            return;
+        }
         tracerInitializer.setup("localhost", 9411, "console-app");
+        setup = true;
     }
 
     @Test
     public void testStartActive() throws InterruptedException {
-        try (ActiveSpan span = spanOps.startActive("hello")) {
+        try (ActiveSpan span = spanOps.startActive("hello sematextains")) {
             span.setTag("app", "console");
+        }
+        Thread.sleep(2000);
+    }
+
+    @Test
+    public void testStartActiveWithParent() throws InterruptedException {
+        try (ActiveSpan parent = spanOps.startActive("parent")) {
+            parent.setTag("app", "console");
+            // simulate I/O op
+            Thread.sleep(500);
+            try (ActiveSpan child = spanOps.startActive("child", parent)) {
+                child.setTag("encoding", "utf-8");
+            }
         }
         Thread.sleep(2000);
     }
