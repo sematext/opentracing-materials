69d2445f8e6a5b4511ca241826f49d85cdbca661
Nedim Šabić <rabbitstack7@gmail.com>
2017-08-21 13:06:21 +0200

    CLI flag to indicate if all endpoints should be traced. logging messages

--- python/opentracing_materials/flask_tracing.py
+++ python/opentracing_materials/flask_tracing.py
@@ -6,6 +6,11 @@
 #    The copyright notice above does not evidence any
 #    actual or intended publication of such source code.
 #
+from argparse import ArgumentParser
+
+import sys
+from logbook import Logger, StreamHandler
+
 from flask import Flask
 from flask_opentracing import FlaskTracer
 
@@ -13,23 +18,56 @@ from opentracing_materials.initializer import TracerInitializer
 
 app = Flask(__name__)
 
-initializer = TracerInitializer('jaeger')
-initializer.setup('localhost', 5775, 'flask')
 
-# initialize `flask` tracer with the instance of
+def __init_logging():
+    StreamHandler(sys.stdout).push_application()
+    return Logger('flask-opentracing')
+
+logger = __init_logging()
+
+tracer_type = 'jaeger'
+tracer_host = 'localhost'
+tracer_port = 5775
+
+initializer = TracerInitializer(tracer_type)
+initializer.setup(tracer_host, tracer_port, 'flask')
+
+logger.info('%s tracer initialized on endpoint %s:%s'
+            % (tracer_type, tracer_host, tracer_port))
+
+parser = ArgumentParser(prog="OpenTracing demonstration for Flask web framework")
+parser.add_argument("--trace-all", help="determines if all requests are traced", action="store_true")
+args = parser.parse_args()
+
+# initialize `flask` tracer with an instance of
 # `jaeger` tracer. With this configuration all requests
 # will be traced. Open your browser and go to http://localhost:5000/api/octi.
 # Hit F5 several times and see how spans for the `octi` endpoint are
 # reported to `Jaeger`.
-flask_tracer = FlaskTracer(
-    initializer.tracer,
-    True,
-    app)
+if args.trace_all:
+    flask_tracer = FlaskTracer(initializer.tracer, True, app)
+
+    @app.route("/api/octi")
+    def octi_endpoint():
+        return "octi"
+    logger.info('Tracing all endpoints. Browse to http://localhost:5000/api/octi')
+else:
+    tracer = FlaskTracer(initializer.tracer)
+
+    @app.route("/api/octi")
+    @tracer.trace("url")
+    def octi_endpoint_traced():
+        # get the current span and attach some tags
+        span = tracer.get_span()
+        span.set_tag("http.method", "GET")
+        return "octi traced"
 
 
-@app.route("/api/octi")
-def octi_endpoint():
-    return "octi"
+    @app.route("/api/octi_notraced")
+    def octi_endpoint_notraced():
+        return "octi not traced"
+
+    logger.info('Tracing single endpoint. Browse to http://localhost:5000/api/octi')
 
 if __name__ == '__main__':
     app.run()
